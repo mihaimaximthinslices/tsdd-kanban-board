2b4960bd4595c323ecf77da7e71cea86a02ac736
mihaimaximthinslices <135857206+mihaimaximthinslices@users.noreply.github.com>
2023-10-18 09:59:35 +0300

    Init FE + BE for Vercel deploy

---
+++ backend/app.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import 'dotenv/config'
+import {Request, Response} from 'express'
+import bodyParser from 'body-parser'
+const app = express()
+import cors from 'cors'
+import cookies from 'cookie-parser'
+app.use(bodyParser.json({ limit: '5mb' }))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(cookies())
+
+app.use(
+  cors({
+    origin: process.env.FE_HOST,
+    credentials: true,
+  }),
+)
+
+
+app.get('/api/hello', (_req: Request, res: Response) => {
+  res.status(200).send('Hello World!')
+})
+
+export default app
