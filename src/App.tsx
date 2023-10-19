1bcfb78d21255300e640f793ea48910d5e30e930
Mihai-Maxim <135857206+mihaimaximthinslices@users.noreply.github.com>
2023-10-19 17:26:41 +0300

    Add the styles and the fonts required for the design

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,44 @@ function App() {
   }, [])
 
   return (
-    <div className="bg-red-400">
-      <p>Hello from dev</p>
-      <h1>{message}</h1>
+    <div className="flex flex-col w-full gap-4">
+      <div className="flex w-full justify-center p-4">
+        <h1>{message}</h1>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h1 className="font-plusJSans font-bold text-headingXL leading-headingXL">
+          Heading (XL)
+        </h1>
+        <h2 className="font-plusJSans font-bold text-headingL leading-headingL">
+          Heading (L)
+        </h2>
+        <h3 className="font-plusJSans font-bold text-headingM leading-headingM">
+          Heading (M)
+        </h3>
+        <h4 className="font-plusJSans font-bold text-headingS leading-headingS tracking-headingS">
+          Heading (S)
+        </h4>
+        <p className="font-plusJSans font-medium text-bodyL leading-bodyL">
+          Body (L) - Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+          Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi
+          neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula
+          sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit
+          nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti. Sed
+          egestas, ante et vulputate volutpat, eros pede semper est, vitae
+          luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing,
+          commodo quis, gravida id, est.
+        </p>
+        <p className="font-plusJSans font-bold text-bodyM leading-bodyM">
+          Body (M) - - Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+          Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi
+          neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula
+          sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit
+          nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti. Sed
+          egestas, ante et vulputate volutpat, eros pede semper est, vitae
+          luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing,
+          commodo quis, gravida id, est.
+        </p>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue2: '#635FC7',
+        blue1: '#A8A4FF',
+        black4: '#000112',
+        black3: '#20212C',
+        black2: '#2B2C37',
+        black1: '#3E3F4E',
+        white4: '#828FA3',
+        white3: '#E4EBFA',
+        white2: '#F4F7FD',
+        white1: '#FFFFFF',
+        red2: '#EA5555',
+        red1: '#FF9898',
+      },
+    },
+    fontSize: {
+      headingXL: '24px',
+      headingL: '18px',
+      headingM: '15px',
+      headingS: '12px',
+      bodyL: '13px',
+      bodyM: '12px',
+    },
+    lineHeight: {
+      headingXL: '30px',
+      headingL: '23px',
+      headingM: '19px',
+      headingS: '15px',
+      bodyL: '23px',
+      bodyM: '12px',
+    },
+    letterSpacing: {
+      headingS: '2.4px',
+    },
+    fontFamily: {
+      plusJSans: ['Plus Jakarta Sans', 'sans-serif'],
+    },
   },
   plugins: [],
 }
-
