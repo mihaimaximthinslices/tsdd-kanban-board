198622631f7887ddc3f2ce8744cb4b604225146a
Mihai-Maxim <135857206+mihaimaximthinslices@users.noreply.github.com>
2023-10-19 11:07:09 +0300

    please run this time

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,9 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000')
-    cy.log('abcd')
 
-    cy.get('h3').contains('http://localhost:3001')
     cy.get('h1').contains('Hello World')
   })
 })
