ad59b7c503835750208be53968cbad7165bd3627
Mihai-Maxim <135857206+mihaimaximthinslices@users.noreply.github.com>
2023-10-27 18:47:21 +0300

    fix error

--- src/components/DashboardSidebar.tsx
+++ src/components/DashboardSidebar.tsx
@@ -40,7 +40,7 @@ export function Toggle() {
           <div
             onClick={() => {
               setEnabled(!enabled)
-              toggleTheme(enabled)
+              toggleTheme()
             }}
             className="w-11 h-6 bg-blue2 rounded-full peer  peer-focus:ring-blue2  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all"
           ></div>
