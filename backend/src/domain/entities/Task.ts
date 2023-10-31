5a8975b72a70692b7c78c34cf1451a9280c16dcd
Mihai-Maxim <135857206+mihaimaximthinslices@users.noreply.github.com>
2023-10-31 19:42:45 +0200

    implement move task functionality

--- backend/src/domain/entities/Task.ts
+++ backend/src/domain/entities/Task.ts
@@ -1,3 +1,5 @@
+import { TaskRepository } from '../repositorties'
+
 export interface Task {
   id: string
   columnId: string
@@ -9,19 +11,29 @@ export interface Task {
   updatedAt: Date
 }
 
-export function orderTasksByLinks(tasks: Task[]) {
-  const map = tasks.reduce((acc: Record<string, Task>, task) => {
+export async function orderTasksByLinks(tasks: Task[], taskRepository: TaskRepository) {
+  const map: Record<string, Task> = tasks.reduce((acc: Record<string, Task>, task) => {
     acc[task.id] = task
     return acc
   }, {})
 
-  const orderedTasks = []
+  const orderedTasks: Task[] = []
+  const visited: Record<string, boolean> = {}
 
   let currentTask = tasks.find((task) => task.taskBeforeId === null)
 
   while (currentTask) {
+    if (visited[currentTask.id]) {
+      const deletePromises = tasks.map((task) => new Promise((resolve) => resolve(taskRepository.delete(task.id))))
+      await Promise.all(deletePromises)
+      return []
+    }
+
+    visited[currentTask.id] = true
     orderedTasks.push(currentTask)
+
     if (!currentTask.taskAfterId) break
+
     currentTask = map[currentTask.taskAfterId]
   }
 
